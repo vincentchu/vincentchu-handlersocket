a5997d3f76fed35f46c85504d42da40e0e9e07e3
vincentchu <vince@posterous.com>
2011-11-14 15:27:40 -0800

    HandlerSocket now gem installable!!!

--- ext/winebarrel-ruby-handlersocket-c19841e47ea2/extconf.rb
+++ ext/winebarrel-ruby-handlersocket-c19841e47ea2/extconf.rb
@@ -9,6 +9,8 @@ base_dir       = File.expand_path( File.dirname(__FILE__) )
 libhs_dir      = File.expand_path( File.join(base_dir, "../HandlerSocket-Plugin-for-MySQL/") )
 libhs_inst_dir = File.join(libhs_dir, "install")
 libhs_client   = File.join(libhs_dir, "libhsclient")
+libhs_libs     = File.join(libhs_inst_dir, "lib")
+libhs_include  = File.join(libhs_inst_dir, "include")
 
 puts "***** Building libhsclient"
 execute_cmd %Q[cd #{libhs_dir} && ./autogen.sh]
@@ -17,8 +19,8 @@ execute_cmd %Q[cd #{libhs_dir} && make]
 execute_cmd %Q[cd #{libhs_dir} && make install]
 execute_cmd %Q[cp -r #{libhs_client} #{base_dir}]
 
-puts "***** FOO!"
-
+puts "***** Building ruby-handlersocket"
+dir_config("hsclient", libhs_include, libhs_libs)
 if have_library('stdc++') and have_library('hsclient')
   create_makefile('handlersocket')
 end
